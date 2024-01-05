802ef2d3839d891a6137d9d5c69586dd28f0420a
Lenzlee Wheeler <lwheeler@luc.edu>
2024-01-04 19:47:53 -0600

    api.js file added

--- client/src/components/RecordButton/RecordButton.jsx
+++ client/src/components/RecordButton/RecordButton.jsx
@@ -8,6 +8,7 @@ import BigResponse from "../BigResponseComponent/BigResponse";
 import ResponseForm from "../ResponseForm/ResponseForm";
 import TopicForm from "../TopicForm/TopicForm";
 import "./RecordButton.scss";
+import { fetchAnswers } from "../../../../server/utils/api";
 
 export default function RecordButton() {
   const [userInput, setUserInput] = useState("");
@@ -22,59 +23,63 @@ export default function RecordButton() {
   const { loading, data } = useQuery(QUERY_ME);
   const userId = data?.me._id;
 
-  const fetchAnswers = async () => {
-    const openAIApiKey = "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";
 
-    const llm = new ChatOpenAI({ openAIApiKey });
+fetchAnswers(promptText)
 
-    const promptTemplate =
-      "You are in a AAC device. Give 10 1-5 word responses to the following topic or question to be used as a button to be pressed by an adult with developmental disabilities. do not start the response with a number separate each new response with a new line: {promptText}";
+  // const fetchAnswers = async () => {
+  //   const openAIApiKey = process.env.API_KEY;
 
-    const responsePrompt = PromptTemplate.fromTemplate(promptTemplate);
-    const responseChain = responsePrompt.pipe(llm);
-    setResponses([]);
+  //   const llm = new ChatOpenAI({ openAIApiKey });
 
-    try {
-      const result = await responseChain.invoke({
-        promptText: userInput,
-      });
+  //   const promptTemplate =
+  //     "You are in a AAC device. Give 10 1-5 word responses to the following topic or question to be used as a button to be pressed by an adult with developmental disabilities. do not start the response with a number separate each new response with a new line: {promptText}";
 
-      setPromptText(userInput);
-      console.log("this is what I got back from chatgpt, ", result);
-      console.log("Question or Topic:", userInput);
-      console.log("Chat GPT Responses:", result.content);
+  //   const responsePrompt = PromptTemplate.fromTemplate(promptTemplate);
+  //   const responseChain = responsePrompt.pipe(llm);
+  //   setResponses([]);
 
-      const newResponses = result.content.split("\n");
-      console.log("This is the array we will map over, ", newResponses);
-      setResponses(newResponses);
+  //   try {
+  //     const result = await responseChain.invoke({
+  //       promptText: userInput,
+  //     });
 
-      const newImageURLs = [];
-      // Loop through responses to fetch and save image URLs
-      newResponses.forEach((response, index) => {
-        const query = response;
+  //     setPromptText(userInput);
+  //     console.log("this is what I got back from chatgpt, ", result);
+  //     console.log("Question or Topic:", userInput);
+  //     console.log("Chat GPT Responses:", result.content);
 
-        const client = createClient(
-          "THj5EwzyfSVYW1UvgByttwmIlcqXDvRS8AmbWtx587POTV86qPqdfd30"
-        );
+  //     const newResponses = result.content.split("\n");
+  //     console.log("This is the array we will map over, ", newResponses);
+  //     setResponses(newResponses);
 
-        client.photos
-          .search({ query, per_page: 1 })
-          .then((data) => {
-            console.log("client.photos data", data);
-            const imageSrc =
-              data.photos.length > 0 ? data.photos[0].src.medium : null;
-            console.log(imageSrc);
-            newImageURLs[index] = imageSrc; // Save each image URL at the corresponding index
-            setImageURLs([...newImageURLs]); // Update the state with the new array of image URLs
-          })
-          .catch((error) => {
-            console.error("Error fetching Pexels data:", error);
-          });
-      });
-    } catch (error) {
-      console.error("Error fetching data from API:", error);
-    }
-  };
+  //     const newImageURLs = [];
+  //     // Loop through responses to fetch and save image URLs
+  //     newResponses.forEach((response, index) => {
+  //       const query = response;
+
+  //       const client = createClient(
+  //         "THj5EwzyfSVYW1UvgByttwmIlcqXDvRS8AmbWtx587POTV86qPqdfd30"
+  //       );
+
+  //       client.photos
+  //         .search({ query, per_page: 1 })
+  //         .then((data) => {
+  //           console.log("client.photos data", data);
+  //           const imageSrc =
+  //             data.photos.length > 0 ? data.photos[0].src.medium : null;
+  //           console.log(imageSrc);
+  //           newImageURLs[index] = imageSrc; // Save each image URL at the corresponding index
+  //           setImageURLs([...newImageURLs]); // Update the state with the new array of image URLs
+  //         })
+  //         .catch((error) => {
+  //           console.error("Error fetching Pexels data:", error);
+  //         });
+  //     });
+  //   } catch (error) {
+  //     console.error("Error fetching data from API:", error);
+  //   }
+  // };
+
 
   return (
     <div>
